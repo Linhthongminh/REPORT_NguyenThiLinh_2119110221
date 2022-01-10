319965d23d947f2896b76df5d5f7da89a63cced1
Linhthongminh <LinhNT1342@gmail.com>
2022-01-10 16:40:39 +0700

    Add content DepartmentBAL

--- LinhThongMinh.BAL/BAL/DepartmentBAL.cs
+++ LinhThongMinh.BAL/BAL/DepartmentBAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinhThongMinh.DTO.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace LinhThongMinh.BAL.BAL
 {
-    class DepartmentBAL
+    public class DepartmentBAL
     {
+        DepartmentDAL dal = new DepartmentDAL();
+        public List<DepartmentDTO> ReadDepartmentList()
+        {
+            List<DepartmentDTO> departments = dal.ReadDepartmentList();
+            return departments;
+        }
     }
 }
